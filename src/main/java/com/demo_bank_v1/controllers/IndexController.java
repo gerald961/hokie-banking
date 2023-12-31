8b3dc2f6f7acb154f560564dd9852da6947c3e14
gerald961 <gerald961@vt.edu>
2023-12-31 00:02:30 -0500

    first full project commit

---
+++ src/main/java/com/demo_bank_v1/controllers/IndexController.java
@@ -0,0 +1,33 @@
+package com.demo_bank_v1.controllers;
+
+import com.demo_bank_v1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public ModelAndView getIndex(){
+        ModelAndView getIndexPage = new ModelAndView("index");
+        getIndexPage.addObject("PageTitle", "Home");
+        System.out.println("In Index Page Controller");
+        return getIndexPage;
+    }
+
+
+    @GetMapping("/error")
+    public ModelAndView getError(){
+        ModelAndView getErrorPage = new ModelAndView("error");
+        getErrorPage.addObject("PageTitle", "Errors");
+        System.out.println("In Error Page Controller");
+        return getErrorPage;
+    }
+
+
+}
